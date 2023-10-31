b646adec76eb93b531c99e0c0be7cf10561fc57a
Olha-buz <134137848+Olha-buz@users.noreply.github.com>
2023-10-31 23:59:58 +0200

    fix1

--- src/03-feedback.js
+++ src/03-feedback.js
@@ -1,55 +1,37 @@
 import throttle from "lodash.throttle";
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.getElementsByName('email'),
-  message: document.getElementsByName('message'),
-};
-const formData = {};
 
-getFormValue();
-
-refs.email.addEventListener('input', throttle(saveInput, 500));
-refs.message.addEventListener('input', throttle(saveInput, 500))
-refs.form.addEventListener('submit', loadInput);
-// window.onload = loadInput();
+form = document.querySelector('.feedback-form');
+email = form.querySelector('email');
+message = form.querySelector('message');
+let formData = {};
 
 function saveInput(event) {
-    formData[event.target.name] = event.target.value;
+    formData[event.target.name] = event.target.value.trim();
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
 function loadInput(event) {
-//    event.preventDefault();
-    if (refs.email.value !== '' || refs.message.value !== '') {
-        console.log(`Email: ${refs.email.value}`);
-        console.log(`Message: ${refs.message.value}`);
-//       event.currentTarget.reset();
-        localStorage.removeItem('feedback-form-state')
-    }
-
+    event.preventDefault();
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    event.target.reset();
  };
 
 function getFormValue() {
-    const savedFormMessage = localStorage.getItem('feedback-form-state');
-    const formValues = JSON.parse(savedFormMessage)
-    
-    if (formValues) {
+      try {
+    const data = localStorage.getItem('feedback-form-state');
+    if (data) {
+    formData = JSON.parse(data);
+        Object.entries(formData).forEach(([key, value]) => {
+            form.elements[key].value = value;
+        });
+      }
+      } catch (error) {
+          console.log(error.message);
+      } 
+};
 
-        const { email, message } = formValues;
-        if ( message && email) {
-        refs.message.value = message;
-        refs.email.value = email;
-        }
-
-        else if (message) {
-            refs.message.value = message;
-    }
-        else if (email) {
-        refs.email.value = email;
-        }
-       
-   }
-   
-}
-
+form.addEventListener('input', throttle(saveInput, 500));
+form.addEventListener('submit', loadInput);
+getFormValue();
